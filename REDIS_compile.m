71c6d2a674ab48b8a54d6781d1878bc582ba013d
Will DeShazer <wdeshazer@users.noreply.github.com>
2024-07-24 08:49:26 -0700

    Replaced mex flat -compatibleArraysDims with -R2018a which breaks 32-bit backward compatibility

--- REDIS_compile.m
+++ REDIS_compile.m
@@ -1,1 +1,1 @@
-mex -compatibleArrayDims -DDEBUG -v REDIS.cpp REDIS_custom.cpp REDIS_wrapper.cpp -L./hiredis -lhiredis
+mex -R2018a -DDEBUG -v REDIS.cpp REDIS_custom.cpp REDIS_wrapper.cpp -L./hiredis -lhiredis
