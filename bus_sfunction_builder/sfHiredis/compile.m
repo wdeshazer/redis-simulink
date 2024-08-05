acb7e3d5b3ddd8c41038d70bbf18f30b7acb6f4b
Will DeShazer <wdeshazer@users.noreply.github.com>
2024-08-04 21:58:41 -0700

    Helper Compile Utilities

---
+++ bus_sfunction_builder/sfHiredis/compile.m
@@ -0,0 +1,24 @@
+clear comp
+
+project = currentProject;
+
+comp = RMCompileClass;
+comp.report()
+
+% comp.iPath
+% comp.hiredis_lib
+
+% comp.headers_list
+% 1×4 cell array
+%   {'hiredis.h'}    {'sds.h'}    {'read.h'}    {'alloc.h'}
+
+% mex -DLIN64 -DUNIX -DNDEBUG -D_USRDLL -O xfft_v9_1_bitacc_mex.cpp ...
+%       -output xfft_v9_1_bitacc_mex -lIp_xfft_v9_1_bitacc_cmodel
+% -L./ 'LDFLAGS=$LDFLAGS -Wl,-rpath,\$ORIGIN'
+
+% mex('-v', '-R2018a', comp.iPath,comp.hfile('hiredis.h'), comp.hiredis_lib)
+
+% compile just the original
+mex -v -R2018a -I./hiredis_mex hiredis_mex/hiredis.h ../../redis_client/hiredis/libhiredis.dylib
+
+mex -v -R2018a -I./hiredis_mex hiredis_mex/hiredis.h ../../redis_client/hiredis/libhiredis.dylib
