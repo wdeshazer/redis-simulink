7848d3323242e559e7f66d7c1743c98106a765ea
Will DeShazer <wdeshazer@users.noreply.github.com>
2024-08-13 17:37:01 -0700

    Simple Simulink Sim

---
+++ sfunction_clean/simple_time_sine_demo/SimpleTimeSinWaveDemo_wrapper.c
@@ -0,0 +1,45 @@
+
+/*
+ * Include Files
+ *
+ */
+#if defined(MATLAB_MEX_FILE)
+#include "tmwtypes.h"
+#include "simstruc_types.h"
+#else
+#define SIMPLIFIED_RTWTYPES_COMPATIBILITY
+#include "rtwtypes.h"
+#undef SIMPLIFIED_RTWTYPES_COMPATIBILITY
+#endif
+
+
+
+/* %%%-SFUNWIZ_wrapper_includes_Changes_BEGIN --- EDIT HERE TO _END */
+#include <math.h>
+/* %%%-SFUNWIZ_wrapper_includes_Changes_END --- EDIT HERE TO _BEGIN */
+
+/*
+ * Create external references here.  
+ *
+ */
+/* %%%-SFUNWIZ_wrapper_externs_Changes_BEGIN --- EDIT HERE TO _END */
+/* extern double func(double a); */
+/* %%%-SFUNWIZ_wrapper_externs_Changes_END --- EDIT HERE TO _BEGIN */
+
+/*
+ * Output function
+ *
+ */
+void SimpleTimeSinWaveDemo_Outputs_wrapper(const real_T *u0,
+			real_T *y0,
+			const int_T y_width,
+			const int_T u_width)
+{
+/* %%%-SFUNWIZ_wrapper_Outputs_Changes_BEGIN --- EDIT HERE TO _END */
+      y0[0] = u0[0];
+      y0[1] = u0[1];
+
+/* %%%-SFUNWIZ_wrapper_Outputs_Changes_END --- EDIT HERE TO _BEGIN */
+}
+
+
